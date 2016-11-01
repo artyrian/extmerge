b7fc291581499d18cbb8bfa24bb8cdfeaa72bb9c
artyrian <artyrian.rahman@gmail.com>
2016-11-01 23:34:31 +0300

    refactor: add scanner for read file and add method for create swap files

--- src/main/java/ru/artyrian/ExtSort.java
+++ src/main/java/ru/artyrian/ExtSort.java
@@ -1,27 +1,24 @@
 package ru.artyrian;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 public class ExtSort {
 
     public static void main(String[] args) {
-        ExtSort.sort("resources/example1.csv", "resources/result.csv", 10);
+        ExtSort.sort("resources/example1.csv", "resources/result.csv", 3);
     }
 
     public static void sort(final String pathInput, final String pathOutput, final int sizeLengthSplit) {
-        Path file = Paths.get(pathInput);
-        try (BufferedReader reader = Files.newBufferedReader(file);
+        try (Scanner scanner = new Scanner(new File(pathInput));
              PrintWriter fileOut = new PrintWriter(pathOutput))
         {
-            fileOut.write(reader.readLine().concat("\n"));   // write header
+            fileOut.write(scanner.nextLine().concat("\n"));   // write header
 
-            List<BufferedIntReader> bufferedReaders = buildSwapFiles(reader, sizeLengthSplit);
+            List<BufferedIntReader> bufferedReaders = buildSwapFiles(scanner, sizeLengthSplit);
             writeMerge(bufferedReaders, fileOut);
         } catch (IOException e) {
             System.err.println("some IOException on write result to file.");
@@ -29,18 +26,18 @@ public class ExtSort {
     }
 
     // read to tmp sorted files wrapped into buffer int readers
-    private static List<BufferedIntReader> buildSwapFiles(final BufferedReader reader, final int lenSplit) throws IOException {
+    private static List<BufferedIntReader> buildSwapFiles(final Scanner scanner, final int lenSplit) throws IOException {
         List<BufferedIntReader> result = new ArrayList<>();
 
         List<Item> items = new ArrayList<>();
         while (true) {
-            String line = reader.readLine();
-            if (items.size() == lenSplit || line == null ) {
-                BufferedIntReader bufferedIntReader = sortToBufferedReader(items);
-                result.add(bufferedIntReader);
+            if (items.size() == lenSplit || !scanner.hasNextLine() ) {
+                Collections.sort(items, (t1, t2) -> t1.getFid() - t2.getFid());
+                File swap = writeToSwap(items);
+                result.add(new BufferedIntReader(swap));
                 items.clear();
 
-                if (line == null) {
+                if (!scanner.hasNextLine()) {
                     if (result.isEmpty()) {
                         throw new RuntimeException("empty input file");
                     }
@@ -48,6 +45,7 @@ public class ExtSort {
                 }
             }
 
+            String line = scanner.nextLine();
             items.add(new Item(line));
         }
 
@@ -87,16 +85,14 @@ public class ExtSort {
             fileOut.write(minItems[minIndex].toString().concat("\n"));
 
             minItems[minIndex] = bufferedReaders.get(minIndex).next();
-            if (minItems[minIndex] != null) {
+            if (minItems[minIndex] == null) {
                 emptyBuffer++;
             }
         }
 
     }
 
-    private static BufferedIntReader sortToBufferedReader(List<Item> items) throws IOException {
-        Collections.sort(items, (t1, t2) -> t1.getFid() - t2.getFid());
-
+    private static File writeToSwap(List<Item> items) throws IOException {
         File tmp = File.createTempFile("_sort", ".tmp");
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(tmp.getAbsoluteFile()))) {
             for (Item item : items) {
@@ -104,10 +100,8 @@ public class ExtSort {
             }
         } catch (IOException e) {
             System.err.format("Tmp IOException: %s%n", e);
-        } finally {
-            items.clear();
         }
 
-        return new BufferedIntReader(tmp);
+        return tmp;
     }
 }
